00e58b06ec517ca47221557310c1a6b66e831baf
Florian Heder <florian.heder@hebotek.at>
2019-06-23 22:36:44 +0200

    cleanup code

--- contracts/Artefact.sol
+++ contracts/Artefact.sol
@@ -2,21 +2,17 @@
 pragma solidity ^0.5.0;
 
 contract Artefact {
-    
-    event EventFired (uint someRandomValue)
-    address[16] owner;
+    event EventFired (uint someRandomValue);
+    address owner;
     bytes32 name;
-    
     constructor(bytes32 _name) public {
-        
         owner = msg.sender;
-
         name = _name;
     }
 
-    function closeContract(uint somePublicRandomValue) pubic returns (uint) {
-      reuqire(msg.sender == owner, "only the owner can close the contract");
-      EventFired(somePublicRandomValue);
+    function closeContract(uint somePublicRandomValue) public returns (uint) {
+      require(msg.sender == owner, "only the owner can close the contract");
+      emit EventFired(somePublicRandomValue);
       return somePublicRandomValue;
     }
 }
--- src/app.js
+++ src/app.js
@@ -9,29 +9,30 @@ App = {
   },
 
   initWeb3: function () {
-   if (typeof web3 !== "undefined") {
-     App.web3Provider = web3.currentProvider;
-   } else {
+    if (typeof web3 !== "undefined") {
+      App.web3Provider = web3.currentProvider;
+    } 
+    else {
      App.web3Provider = web3.eth.accounts; //create local variable for easy access
-   }
-   return App.initContract();
+    }
+    return App.initContract();
   },
+
   initContract: function () {
     Artefact.new({from: App.web3Provider[0]})
-    .then((contract) =>{
-      contract.EventBeingWatched().watch((err,response) => {
+    .then( (contract) => {
+      contract.EventBeingWatched().watch( (err,response) => {
         App.status.innerHTML = 'The event was successfully triggered with the value ' + response.args.someRandomValue;
-      }
       });
-      setTimeout(() => {
+      setTimeout( () => {
         contract.callSmartContractFunction  (Math.random(), {from: App.web3Provider[0]});
       }, 3000);
     })
     .catch( (err)  => {
       console.log('some error occured.');
     });
+  },
 };
-
 
 window.onload = function () {
   App.init();
